cdef99efbcf9bde7b1dfa9b01ddaca36c204203d
Jorge Rossi <jorgefzrossi@gmail.com>
2020-06-21 10:20:02 -0300

    feat: add function to generate card_hash

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,50 @@
-export default {
-  multiply(a: number, b: number) {
-    return Promise.resolve(a * b);
-  },
-};
+import axios from 'axios';
+import qs from 'qs';
+import { RSA } from 'react-native-rsa-native';
+
+interface CardHashKey {
+  id: number;
+  publicKey: string;
+}
+
+interface Card {
+  number: string;
+  holderName: string;
+  expirationDate: string;
+  cvv: string;
+}
+
+async function requestCardHashKey(encryptionKey: string): Promise<CardHashKey> {
+  const {
+    data: { id, public_key: publicKey },
+  } = await axios.get('https://api.pagar.me/1/transactions/card_hash_key', {
+    params: { encryption_key: encryptionKey },
+  });
+
+  return {
+    id,
+    publicKey,
+  };
+}
+
+function generateQueryString(card: Card) {
+  return qs.stringify({
+    card_number: card.number.replace(/[^0-9]/g, ''),
+    card_holder_name: card.holderName,
+    card_expiration_date: card.expirationDate.replace(/[^0-9]/g, ''),
+    card_cvv: card.cvv.replace(/[^0-9]/g, ''),
+  });
+}
+
+async function generateCardHash(
+  card: Card,
+  encryptionKey: string
+): Promise<string> {
+  const cardHashKey = await requestCardHashKey(encryptionKey);
+  const querysSring = generateQueryString(card);
+  const encrypted = await RSA.encrypt(querysSring, cardHashKey.publicKey);
+
+  return `${cardHashKey.id}_${encrypted}`;
+}
+
+export default generateCardHash;
